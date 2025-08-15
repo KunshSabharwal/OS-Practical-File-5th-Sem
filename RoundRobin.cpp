c45589fd7b1dfa5f8f31f37b9df2de26bea0695c
Kunsh Sabharwal <sabharwalkunsh05@gmail.com>
2025-08-15 20:13:12 +0530

    added round robin program exp 4(b)

---
+++ RoundRobin.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+void findWaitingTime(int processes[], int n, int bt[], int wt[], int quantum)
+{
+    int rem_bt[n];
+    for (int i = 0; i < n; i++)
+    {
+        rem_bt[i] = bt[i];
+    }
+
+    int t = 0;
+    while (1)
+    {
+        int done = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (rem_bt[i] > 0)
+            {
+                done = 0;
+                if (rem_bt[i] > quantum)
+                {
+                    t += quantum;
+                    rem_bt[i] -= quantum;
+                }
+                else
+                {
+                    t += rem_bt[i];
+                    wt[i] = t - bt[i];
+                    rem_bt[i] = 0;
+                }
+            }
+        }
+        if (done == 1)
+            break;
+    }
+}
+
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[])
+{
+    for (int i = 0; i < n; i++)
+    {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+void findAverageTime(int processes[], int n, int bt[], int quantum)
+{
+    int wt[n], tat[n];
+    findWaitingTime(processes, n, bt, wt, quantum);
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    printf("Processes   Burst Time   Waiting Time   Turnaround Time\n");
+    int total_wt = 0, total_tat = 0;
+    for (int i = 0; i < n; i++)
+    {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("   %d ", (i + 1));
+        printf("          %d ", bt[i]);
+        printf("           %d ", wt[i]);
+        printf("                %d\n", tat[i]);
+    }
+
+    printf("Average waiting time = %.2f\n", (float)total_wt / (float)n);
+    printf("Average turnaround time = %.2f\n", (float)total_tat / (float)n);
+}
+
+int main()
+{
+    int processes[] = {1, 2, 3};
+    int n = sizeof processes / sizeof processes[0];
+    int burst_time[] = {10, 5, 8};
+    int quantum = 2;
+
+    findAverageTime(processes, n, burst_time, quantum);
+    return 0;
+}
